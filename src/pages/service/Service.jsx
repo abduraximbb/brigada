d1002840a806d40bf2379d15893bc09adb6d22d9
mushtariy93 <abdurashidovamushtariykumush@gmail.com>
2024-12-03 23:22:31 +0500

    Page Service

---
+++ src/pages/service/Service.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "./Hero";
+import Process from "./Process";
+import Card from "./Card";
+
+
+const Service = () => {
+  return (
+    <>
+      <Hero />
+      <Process />
+      <Card />
+    </>
+  );
+};
+
+export default Service;
