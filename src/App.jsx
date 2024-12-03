31f7c510a319f5e3b263562b6da1698909a7ceed
Abduraxim Berdiqobilov <abduraximberdiqobilov@Abduraxims-MacBook-Air.local>
2024-12-03 11:06:12 +0500

    sklet project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
 
 function App() {
 
   return (
     <>
-     <div><h2>Brigada team</h2></div>
+      <Header />
+      <main>
+        <h2>New project</h2>
+        <ul>
+          <li>Azizbek-Home</li>
+          <li>Mushtariy opa-Service</li>
+          <li>Abduraxim - Company</li>
+          <li>Abdulvahhob-Career</li>
+        </ul>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
